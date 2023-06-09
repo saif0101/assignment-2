05a557e5351cf9ec507a13ae5fa170e7e9b61095
uihut-saif70 <113232028+uihut-saif70@users.noreply.github.com>
2023-06-09 11:37:33 +0600

    okay

--- src/components/Home/Products.vue
+++ src/components/Home/Products.vue
@@ -4,7 +4,7 @@
      <div class="flex flex-col lg:flex-row">
         <div class="basis-1/3">
             <h2 class="text-[28px] leading-[36px] md:text-[36px] md:leading-[48px] lg:text-[40px] lg:leading-[60px] font-semibold">Products <br/> A-Z</h2>
-            <p>hellogit</p>
+           
 
         </div>
             
